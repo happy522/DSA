022bc9c5b2d1d97e52727f777df24244c8413136
priyanshu agarwal <60876324+priyanshuagarwal7@users.noreply.github.com>
2021-10-07 12:16:35 +0530

    Update fibonacci.cpp

--- Recursion/fibonacci.cpp
+++ Recursion/fibonacci.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int arr[100001] = {-1};
+
 int fibonacci(int n)
 {
-    if(n==0 || n==1)
-    return n;
-    else
-    return (fibonacci(n-1)+fibonacci(n-2));
+    if(arr[n]!=-1){
+        return arr[n];
+    }
+    if(n==0||n==1){
+        return 1;
+    }
+    arr[n] = fibonacci(n-1) + fibonacci(n-2);
+    return arr[n];
 }
 
 int main() {
