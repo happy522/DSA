13fd80b36d5d50bcbfc73ac3e32599742994e124
khushi-research <116890889+khushi-research@users.noreply.github.com>
2022-10-28 19:03:57 +0530

    Create CHEFCHOCO.cpp

---
+++ codechef/CHEFCHOCO.cpp
@@ -0,0 +1,18 @@
+/*Chef wants to gift CC chocolates to Botswal on his birthday. However, he has only XX chocolates with him.
+The cost of 11 chocolate is YY rupees.
+
+Find the minimum money in rupees Chef needs to spend so that he can gift CC chocolates to Botswal.*/
+#include <iostream>
+using namespace std;
+
+#include <bits/stdc++.h>
+int main() {
+    int n;
+    cin>>n;
+    while(n--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        
+    }
+	return 0;
+}
